f570efb88fd139528e5c8290ceca31e6cc61de0a
이소라 Sora Lee <46148636+srlee010@users.noreply.github.com>
2023-01-09 22:36:22 +0900

    Create #4 for Loops.swift

---
+++ #4 for Loops.swift
@@ -0,0 +1,18 @@
+// Day 4🐿
+// 1. For loops
+
+let count = 1...10
+for number in count {
+    print("Number is \(number)")
+}
+
+
+let albums = ["OMG", "Ditto", "Cookie"]
+for album in albums {
+    print("\(album)는 뉴진스의 노래입니다.")
+}
+
+//OMG는 뉴진스의 노래입니다.
+//Ditto는 뉴진스의 노래입니다.
+//Cookie는 뉴진스의 노래입니다.
+
