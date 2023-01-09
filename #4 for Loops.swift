ede30531fafb9c9cf8a4149b5b4c6c60fcffb3a3
이소라 Sora Lee <46148636+srlee010@users.noreply.github.com>
2023-01-09 22:36:57 +0900

    Update #4 for Loops.swift

--- #4 for Loops.swift
+++ #4 for Loops.swift
@@ -16,3 +16,20 @@ for album in albums {
 //Ditto는 뉴진스의 노래입니다.
 //Cookie는 뉴진스의 노래입니다.
 
+for _ in 0...3 {
+    print("Loop 반복문 테스트") // 총 4번 반복함
+}
+
+for song in 4...6 {
+    print("뉴진스의 \(song)번째 노래")
+}
+// 뉴진스의 4번째 노래
+// 뉴진스의 5번째 노래
+// 뉴진스의 6번째 노래
+
+// 잘못된 예시
+// loop에서는 tuple을 이렇게 사용할 수 없다.
+ var speeds = (65, 58, 72)
+ for speed in speeds {
+     print("You were driving at \(speed)km/h.")
+}
