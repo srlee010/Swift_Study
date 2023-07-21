f2523d02993db4f253eb7c139a009b80ccc7b17d
Sora Lee <srlee010@gmail.com>
2023-07-22 00:24:44 +0900

    3rd commit

--- L3 Demo 2/ContentView.swift
+++ L3 Demo 2/ContentView.swift
@@ -17,7 +17,7 @@ import SwiftUI
                 Image("macImage")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .cornerRadius(20)
+                    .cornerRadius(15)
                 
                 HStack {
                     
