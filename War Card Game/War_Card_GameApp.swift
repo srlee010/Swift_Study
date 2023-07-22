d2d7611900549fa3c061dd83d4a893471a2048b4
Sora Lee <srlee010@gmail.com>
2023-07-22 10:29:11 +0900

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Sora Lee on 2023/07/22.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
